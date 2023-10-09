0c78362e42a8004def7d2b18fcac32ee59eacdbb
Alvaro <alvarodc99@hotmail.com>
2023-10-09 15:05:19 -0300

    estructura base

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
+const URL = 'https://fakestoreapi.com/products'
+const container = document.getElementById("products");
 
-let apiUrl = 'https://fakestoreapi.com/products';
-let arrayProducts = [];
-document.addEventListener('DOMContentLoaded', function () { 
-    fetch(apiUrl) 
-        .then(response => response.json()) 
-        .then(data => { 
-            arrayProducts = data;
-            console.log(arrayProducts);
-        }) 
-        .catch(error => { 
-            console.log('Error al obtener los datos:', error); 
-        }) 
-});
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  fetchData(URL)
+})
+
+async function fetchData(url) {
+  try {
+      const response = await fetch(url);
+      const data = await response.json();
+      products = data
+      showProducts(products)
+  } catch (error) {
+      console.error('Error al obtener datos:', error);
+  }
+}
+
+function showProducts(array) {
+for (const item of array) {
+  const divDeProducto = document.createElement('div');
+  divDeProducto.classList.add('row', 'list-group-item', 'd-flex', 'justify-content-between', 'mt-3');
+  const productHTML = `
+      <div class="col-3">
+          <img src="${item.image}" class="img-thumbnail">
+      </div>
+      <div class="col-6">
+          <h3>${item.title} - $ ${item.price}</h3>
+          <p>${item.description}</p>
+      </div>
+      <div class="col-3 text-muted text-end">
+          <small>${item.rating.count} in stock</small>
+      </div>
+  `;
+  divDeProducto.innerHTML = productHTML;
+
+  container.appendChild(divDeProducto);
+
+  divDeProducto.addEventListener('click', function () {
+      redirectToProductInfo(item.id);
+  });
+}
+}
+
+
